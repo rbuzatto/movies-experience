21dc1dd36eeb96b5d42a12cf3a9bf5c514d4cc85
rbuzatto <rafael.buzatto@energage.com>
2025-03-16 13:49:19 -0300

    set middleware

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { getData } from './services/httpServer'
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
+
+  if (token) return NextResponse.next()
+
+  try {
+    const resp = await getData<{ token: string }>('/auth/token')
+    const newToken = resp.token
+    if (!newToken) {
+      return NextResponse.next()
+    }
+
+    const response = NextResponse.next()
+    response.cookies.set({
+      name: 'token',
+      value: newToken,
+      httpOnly: false,
+    })
+
+    return NextResponse.rewrite(request.nextUrl, response)
+  } catch (error) {
+    return NextResponse.next()
+  }
+}
