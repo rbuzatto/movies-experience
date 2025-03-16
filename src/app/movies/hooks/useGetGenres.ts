e0f68089d943019551f05b1d289203f59ca56ab3
rbuzatto <rafael.buzatto@energage.com>
2025-03-16 20:43:44 -0300

    queries

---
+++ src/app/movies/hooks/useGetGenres.ts
@@ -0,0 +1,27 @@
+import { getData } from '@/services/httpClient'
+import { useQuery } from '@tanstack/react-query'
+
+type Movie = {
+  id: string
+  posterUrl: string
+  rating: number
+  title: string
+}
+
+type Genre = {
+  id: string
+  title: string
+  movies: Pick<Movie, 'id'>[]
+}
+
+type GenreData = {
+  data: Genre[]
+  totalPages: number
+}
+
+export const useGetGenres = () => {
+  return useQuery<GenreData>({
+    queryKey: ['genre', 'movies'],
+    queryFn: () => getData('/genres/movies'),
+  })
+}
---
+++ src/app/movies/hooks/useGetMovies.ts
@@ -0,0 +1,43 @@
+import { getData } from '@/services/httpClient'
+import { useQuery } from '@tanstack/react-query'
+
+type Movie = {
+  id: string
+  posterUrl: string
+  rating: number
+  title: string
+}
+
+type MovieQuery = {
+  data: Movie[]
+  totalPages: number
+}
+
+type Genre = {
+  id: string
+  title: string
+  movies: Pick<Movie, 'id'>[]
+}
+
+type GenreData = {
+  data: Genre[]
+  totalPages: number
+}
+
+type UseGetMoviesProps = {
+  search: string
+  genre: string
+  currentPage: number
+}
+
+export const useGetMovies = ({ search, genre, currentPage }: UseGetMoviesProps) => {
+  return useQuery({
+    queryKey: ['movies', search, genre, currentPage],
+    queryFn: () =>
+      getData<MovieQuery>('/movies?search={0}&genre={1}&page={2}', [
+        search,
+        genre,
+        currentPage.toString(),
+      ]),
+  })
+}
