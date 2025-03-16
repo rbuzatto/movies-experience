fff94d8fab6f003fe98a4bf14ebba8bcaa09bf78
rbuzatto <rafael.buzatto@energage.com>
2025-03-16 18:55:50 -0300

    page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-export default function Home() {
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export default function PageHome() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">Foo</main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-    </div>
+    <main className="flex flex-col items-center justify-center h-screen space-y-4 bg-gray-50">
+      <h1 className="text-5xl font-bold text-cyan-700">
+        Get Amazed and find the pictures you love!
+      </h1>
+      <p className="text-xl text-gray-600 w-3/4 text-center">
+        Find your favorite movie and enjoy the experience. The best movies are here! A unique
+        collection of movies you love to watch. Enjoy the experience!
+      </p>
+      <Button variant={'default'} className="mt-4 text-3xl">
+        <Link href={'/movies'}>Start</Link>
+      </Button>
+    </main>
   )
 }
