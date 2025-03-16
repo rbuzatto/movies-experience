e6484b9d00d5ea49db992930e0c4041c5daaa60b
rbuzatto <rafael.buzatto@energage.com>
2025-03-16 20:44:10 -0300

    add padding to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function PageHome() {
         Find your favorite movie and enjoy the experience. The best movies are here! A unique
         collection of movies you love to watch. Enjoy the experience!
       </p>
-      <Button variant={'default'} className="mt-4 text-3xl">
+      <Button variant={'default'} className="mt-4 p-8 text-3xl">
         <Link href={'/movies'}>Start</Link>
       </Button>
     </main>
